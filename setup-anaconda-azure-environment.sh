9d96cd9d4fdb96d3971348e27ecc3f7ffdb7814c
Teodor Wojcik <teodor.wojcik@gmail.com>
2017-11-18 14:34:12 +0100

    add setup-anaconda-azure-environment.sh

---
+++ setup-anaconda-azure-environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh
+source ~/.bashrc
+pip install NiBabel
+pip install tensorflow
+pip install keras
